29043ba5a5c0279083f684e14a74a9d269380f8c
VlaRu <dn210690nvs@gmail.com>
2024-09-20 13:47:17 +0200

    fix: delete unnecessary files

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -3,43 +3,34 @@ const minutesElement = document.querySelector('.time_minute');
 const secondsElement = document.querySelector('.time_seconds');
 
 export function startTimer() {
-  // Function to start the countdown
   function countdown() {
-    // Set the duration for 30 minutes in milliseconds
-    const duration = 30 * 60 * 1000; // 30 minutes in milliseconds
 
-    // Get the current time in milliseconds
+    const duration = 30 * 60 * 1000;
+
     const now = new Date().getTime();
 
-    // Set the target time to 30 minutes from now
     const targetTime = now + duration;
 
     const interval = setInterval(() => {
-      // Get the current time again
       const currentTime = new Date().getTime();
 
-      // Calculate the remaining time in milliseconds
       let remainingTime = targetTime - currentTime;
 
-      // Check if the time is up
       if (remainingTime <= 0) {
         clearInterval(interval);
-        countdown(); // Restart the timer
+        countdown();
         return;
       }
 
-      // Calculate hours, minutes, and seconds
       let hours = Math.floor(remainingTime / (1000 * 60 * 60));
       let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
       let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-      // Update the timer display
       hoursElement.textContent = String(hours).padStart(2, '0');
       minutesElement.textContent = String(minutes).padStart(2, '0');
       secondsElement.textContent = String(seconds).padStart(2, '0');
-
     }, 1000);
   }
 
-  countdown(); // Start the first countdown
+  countdown();
 }
--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
-import {showLesson} from "./modules/show-lesson.js";
 import {startTimer} from "./modules/timer.js";
 import {currentDay} from "./modules/about-data.js";
 import {showAccordion} from "./modules/show-answer.js";
 import { scrollUp } from "./modules/arrow-scroll.js";
 import { burgerMenu, toggleBurgerMenu } from "./modules/burger-menu.js";
 
-showLesson();
 startTimer();
 showAccordion();
 window.addEventListener("scroll", scrollUp);
